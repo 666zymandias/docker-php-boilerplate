96cce392b10fc4ddad5cb924c94d6217c118d563
Felipe Santos <hydfelipe@gmail.com>
2025-04-01 13:35:36 +0200

    [verde] - vaciar lista con 3 productos

--- tests/ListaCompraTest.php
+++ tests/ListaCompraTest.php
@@ -36,9 +36,26 @@ class ListaCompraTest extends TestCase {
 
         $respuesta = $listaCompra->tratarInstruccion('eliminar pan');
         $this->assertEquals('cafe x2, ', $respuesta);
-
         $respuesta = $listaCompra->tratarInstruccion('eliminar harina');
         $this->assertEquals('El producto seleccionado no existe', $respuesta);
+
+    }
+
+    /**
+     * @test
+     */
+    public function vaciarListaCompra() {
+        $listaCompra = new ListaCompra();
+
+        $respuesta = $listaCompra->tratarInstruccion('añadir pan 4');
+        $this->assertEquals('pan x4, ', $respuesta);
+        $respuesta = $listaCompra->tratarInstruccion('añadir cafe 2');
+        $this->assertEquals('pan x4, cafe x2, ', $respuesta);
+        $respuesta = $listaCompra->tratarInstruccion('añadir harina');
+        $this->assertEquals('pan x4, cafe x2, harina x1, ', $respuesta);
+
+        $respuesta = $listaCompra->tratarInstruccion('vaciar');
+        $this->assertEquals('', $respuesta);
 
     }
 }
